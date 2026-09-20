1a246fb82cc12a145df261342480168dec3230e8
Jamaludin-Mujaddidi <jamaludin.mujaddidi@gmail.com>
2026-09-21 03:30:39 +0430

    Add keyword arguments example

--- 09-functions/02-parameters-and-arguments.py
+++ 09-functions/02-parameters-and-arguments.py
@@ -1,5 +1,17 @@
 # Parameters and Arguments
-def greet (name):
-    print (f"Hello Mr. {name}")
+
+
+def greet(name):
+    print(f"Hello Mr. {name}.")
+
 
 greet("John")
+
+
+# Keyword Arguments
+
+def introduce(name, age):
+    print(f"This is {name} and he is {age} years old.")
+
+
+introduce(name="John", age=26)
